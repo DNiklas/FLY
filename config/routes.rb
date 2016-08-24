a4b3ef8ff7086ae2326c5daff3360b033026dda0
DNiklas <niklas.dietz@gmx.net>
2016-08-24 11:52:21 +0200

    5.1 first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+    get '/products/:id', to: 'products#show'
+
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'static_pages/about'
 
@@ -7,5 +9,5 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-root 'static_pages#landing_page'
+root 'static_pages/about'
 end
